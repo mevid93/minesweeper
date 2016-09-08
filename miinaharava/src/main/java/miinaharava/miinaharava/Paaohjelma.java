240cd84ee04121ae34d9e0a5137d63d483b06363
Martin Vidjeskog <Martin Vidjeskog>
2016-09-08 13:13:24 +0300

    Tiedostojen kanssa säädetty

---
+++ miinaharava/src/main/java/miinaharava/miinaharava/Paaohjelma.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miinaharava.miinaharava;
+
+/**
+ *
+ * @author Marski
+ */
+public class Paaohjelma {
+    
+    public static void main(String[]args){
+        System.out.println("Kaikki tehty.");
+    }
+    
+}
